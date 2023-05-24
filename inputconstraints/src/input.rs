a39d47d17ecc33f670bc9d0816c19a274b5a824a
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2023-05-24 18:11:50 -0400

    inputfilter - Progress on simplified implementation - Added initial implementations of '_validate_against_validators', and '_validate_against_arc_validators' - **Note:** 'validators' fields should accept moved, and/or, 'Arc<&dyn Fn() -> ...>', functions.

--- inputconstraints/src/input.rs
+++ inputconstraints/src/input.rs
@@ -1,14 +1,14 @@
 use std::borrow::Cow;
-use std::fmt::{Debug, Display};
+use std::fmt::{Debug};
 use std::sync::Arc;
-use regex::Regex;
-use crate::input::ValidityState::{CustomError, PatternMismatch, RangeOverflow, RangeUnderflow, TooLong, TooShort, Valid, ValueMissing};
+
+use crate::input::ConstraintViolation::{ValueMissing};
 use crate::types::InputValue;
 
 pub type ConstraintViolationMsg = String;
 
 #[derive(PartialEq, Debug)]
-pub enum ValidityState {
+pub enum ConstraintViolation {
   CustomError,
   PatternMismatch,
   RangeOverflow,
@@ -33,10 +33,10 @@ pub type ConstraintCheck<T> =
 dyn Fn(T) -> bool + Send + Sync;
 
 pub type Message = String;
-pub type ValidationResult = Result<(), ValidityState>;
-pub type ValidationResultTuple = (ValidityState, Message);
+pub type ValidationResult = Result<(), ConstraintViolation>;
+pub type ValidationResultTuple = (ConstraintViolation, Message);
 pub type Validator<'a, T> = &'a (
-dyn Fn(T) -> Option<Vec<(ValidityState, Message)>> + Send + Sync
+dyn Fn(T) -> Option<(ConstraintViolation, Message)> + Send + Sync
 );
 pub type Filter<'a, T> = &'a (dyn Fn(Cow<T>) -> Cow<T> + Send + Sync);
 
@@ -53,8 +53,11 @@ pub struct Input<'a, T> where
   #[builder(default = "false")]
   pub required: bool,
 
-  #[builder(default = "None")]
-  pub validators: Option<Arc<Vec<Arc<Validator<'a, T>>>>>,
+  #[builder(setter(strip_option), default = "None")]
+  pub validators: Option<Vec<Validator<'a, T>>>,
+
+  #[builder(setter(strip_option), default = "None")]
+  pub arc_validators: Option<Arc<Vec<Arc<Validator<'a, T>>>>>,
 }
 
 impl<T: InputValue> Input<'_, T> {
@@ -64,29 +67,49 @@ impl<T: InputValue> Input<'_, T> {
       name: None,
       required: false,
       validators: None,
+      arc_validators: None,
     }
   }
 
-  pub fn validate(&self, value: Option<T>) -> Result<(), Vec<(ValidityState, Message)>> {
+  fn _validate_against_arc_validators(&self, value: T) -> Option<Vec<(ConstraintViolation, Message)>> {
+    self.arc_validators.as_deref().map(|vs| {
+      vs.iter().fold(vec![], |mut agg, f| {
+        let _fn: Arc<Validator<T>> = Arc::clone(f);
+
+        match (_fn)(value.clone()) {
+          Some(message_tuples) => {
+            agg.push(message_tuples);
+            agg
+          }
+          None => agg
+        }
+      })
+    })
+  }
+
+  fn _validate_against_validators(&self, value: T) -> Option<Vec<(ConstraintViolation, Message)>> {
+    self.validators.as_deref().map(|vs| {
+      vs.iter().fold(vec![], |mut agg, f| {
+
+        match (f)(value.clone()) {
+          Some(message_tuples) => {
+            agg.push(message_tuples);
+            agg
+          }
+          None => agg
+        }
+      })
+    })
+  }
+
+  pub fn validate(&self, value: Option<T>) -> Result<(), Vec<(ConstraintViolation, Message)>> {
     match &value {
       None => if !self.required {
         Ok(())
       } else {
         Err(vec![(ValueMissing, "Value missing".to_string())])
       },
-      Some(v) => match self.validators.as_deref().map(|vs| {
-        vs.iter().fold(vec![], |mut agg, f| {
-          let _fn: Arc<Validator<T>> = Arc::clone(f);
-
-          match (_fn)(v.clone()) {
-            Some(mut message_tuples) => {
-              agg.append(message_tuples.as_mut());
-              agg
-            }
-            None => agg
-          }
-        })
-      }) {
+      Some(v) => match self._validate_against_arc_validators(v.clone()) {
         None => Ok(()),
         Some(_msgs) => if _msgs.len() > 0 {
           Err(_msgs)
@@ -95,5 +118,77 @@ impl<T: InputValue> Input<'_, T> {
         }
       }
     }
+  }
+}
+  
+#[cfg(test)]
+mod test {
+  use std::sync::Arc;
+  use regex::Regex;
+  use crate::input::{InputBuilder, ConstraintViolation};
+  use std::error::Error;
+  use crate::input::ConstraintViolation::PatternMismatch;
+
+  #[test]
+  fn test_input_builder() -> Result<(), Box<dyn Error>> {
+    let unsized_less_100 = |x: u32| -> Option<(ConstraintViolation, String)> {
+      if x >= 100 {
+        return Some((ConstraintViolation::RangeOverflow, "Value greater than 100".to_string()));
+      }
+      None
+    };
+
+    let ymd_regex = Regex::new(r"^\d{1,4}-\d{1,2}-\d{1,2}$")?;
+    let ymd_mismatch_msg = |s: &str| -> String {
+      format!("{} doesn't match pattern {}", s, ymd_regex.as_str())
+    };
+    let ymd_check = |s: &str| -> Option<(ConstraintViolation, String)> {
+      if !ymd_regex.is_match(s) {
+        return Some(
+          (ConstraintViolation::PatternMismatch,
+           ymd_mismatch_msg(s))
+        );
+      }
+      None
+    };
+
+    let less_than_100_input = InputBuilder::<u32>::default()
+      .arc_validators(Arc::new(vec![Arc::new(&unsized_less_100)]))
+      .build()?;
+
+    let yyyy_mm_dd_input = InputBuilder::<&str>::default()
+      .arc_validators(Arc::new(vec![Arc::new(&ymd_check)]))
+      .build()?;
+
+    match less_than_100_input.validate(None) {
+      Err(errs) => panic!("Expected Ok(());  Received Err({:#?})", &errs),
+      Ok(()) => ()
+    }
+
+    let value = "1000-99-999";
+
+    // Mismatch check
+    match yyyy_mm_dd_input.validate(Some(value)) {
+      Ok(_) => panic!("Expected Err(...);  Received Ok(())"),
+      Err(tuples) => {
+        assert_eq!(tuples[0].0, PatternMismatch);
+        assert_eq!(tuples[0].1, ymd_mismatch_msg(value).as_str());
+      }
+    }
+
+    // Valid check
+    match yyyy_mm_dd_input.validate(None) {
+      Err(errs) => panic!("Expected Ok(());  Received Err({:#?})", &errs),
+      Ok(()) => ()
+    }
+
+    // Valid check 2
+    let value = "1000-99-99";
+    match yyyy_mm_dd_input.validate(Some(value)) {
+      Err(errs) => panic!("Expected Ok(());  Received Err({:#?})", &errs),
+      Ok(()) => ()
+    }
+
+    Ok(())
   }
 }
