9f8914d4d8acf107ae38865be0cc2eb260497512
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2023-09-01 13:00:04 -0400

    issue-#3 - Progress on 'slug' filter - Requires tests and verification of 'Fn*' trait impls.

--- inputfilter/src/filter/mod.rs
+++ inputfilter/src/filter/mod.rs
@@ -1,3 +1,5 @@
 pub mod slug;
+pub mod xml_entities;
 
 pub use slug::*;
+pub use xml_entities::*;
--- inputfilter/src/filter/slug.rs
+++ inputfilter/src/filter/slug.rs
@@ -1,8 +1,15 @@
 use std::borrow::Cow;
+use std::marker::PhantomData;
 use std::sync::OnceLock;
 use regex::Regex;
 
 static SLUG_REGEX: OnceLock<Regex> = OnceLock::new();
+static SLUG_FILTER_REGEX_STR: &str = r"(?i)[^\w\-]";
+
+/// Returns the static regex used for filtering a string to slug.
+pub fn get_slug_regex() -> &'static Regex {
+  SLUG_REGEX.get_or_init(|| Regex::new(SLUG_FILTER_REGEX_STR).unwrap())
+}
 
 /// Normalizes given string into a slug - e.g., a string matching /^\w[\w\-]{0,198}\w?$/
 ///
@@ -13,16 +20,60 @@ static SLUG_REGEX: OnceLock<Regex> = OnceLock::new();
 /// assert_eq!(to_slug(Cow::Borrowed("Hello World")), "hello-world");
 /// ```
 pub fn to_slug(xs: Cow<str>) -> Cow<str> {
-  let rx = SLUG_REGEX.get_or_init(|| Regex::new(r"(?i)[^\w\-]").unwrap());
-  let rslt = rx.replace_all(xs.as_ref(), "-")
+  _to_slug(get_slug_regex(), 200, xs)
+}
+
+pub fn _to_slug<'a, 'b>(pattern: &Regex, max_length: usize, xs: Cow<'a, str>) -> Cow<'b, str> {
+  let rslt = pattern.replace_all(xs.as_ref(), "-")
     .to_lowercase()
     .trim_matches('-')
     .to_string();
 
-  if rslt.len() > 200 {
+  if rslt.len() > max_length {
     Cow::Owned(rslt[..201].to_string())
   } else {
     Cow::Owned(rslt)
   }
 }
 
+/// Configurable version of `to_slug()` - allows for setting the max_length.
+#[derive(Clone, Debug, Default, Builder)]
+pub struct SlugFilter<'a> {
+  #[builder(setter(into), default = "200")]
+  pub max_length: usize,
+
+  _phantom_field: PhantomData<&'a str>
+}
+
+impl<'a> SlugFilter<'a> {
+  pub fn new(max_length: usize) -> Self {
+    SlugFilter {
+      max_length,
+      _phantom_field: Default::default(),
+    }
+  }
+
+  pub fn filter<'b: 'a>(&self, xs: Cow<'a, str>) -> Cow<'b, str> {
+    _to_slug(get_slug_regex(), self.max_length, xs)
+  }
+}
+
+impl<'a> FnOnce<(Cow<'a, str>,)> for SlugFilter<'_> {
+  type Output = Cow<'a, str>;
+
+  extern "rust-call" fn call_once(self, args: (Cow<'a, str>,)) -> Self::Output {
+    self.filter(args.0)
+  }
+}
+
+impl<'a, 'b> Fn<(Cow<'a, str>,)> for SlugFilter<'_> {
+  extern "rust-call" fn call(&self, args: (Cow<'a, str>,)) -> Self::Output {
+    self.filter(args.0)
+  }
+}
+
+impl<'a, 'b> FnMut<(Cow<'a, str>,)> for SlugFilter<'_> {
+  extern "rust-call" fn call_mut(&mut self, args: (Cow<'a, str>,)) -> Self::Output {
+    self.filter(args.0)
+  }
+}
