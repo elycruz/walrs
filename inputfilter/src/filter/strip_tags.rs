7d50c7e2de1ae44ca48f95c0ba6b25842f276714
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2023-09-04 10:02:49 -0400

    issue-#4 - Added (untested) implementation of 'StripTags' filter.

--- inputfilter/src/filter/strip_tags.rs
+++ inputfilter/src/filter/strip_tags.rs
@@ -1,15 +1,45 @@
-use std::collections::{HashMap, HashSet};
+use std::borrow::Cow;
+use ammonia;
 
-pub struct StripTags {
-  /// Tags to strip.
-  tags: Option<Vec<String>>,
+pub struct StripTags<'a> {
+  pub ammonia: Option<ammonia::Builder<'a>>,
+}
 
-  /// Map of tags, and attributes to allow on them.
-  tags_and_attribs: Option<HashMap<String, HashSet<String>>>,
+impl<'a> StripTags<'a> {
+  pub fn new() -> Self {
+    Self {
+      ammonia: None,
+    }
+  }
 
-  /// Attributes to strip.
-  attribs: Option<Vec<String>>,
+  pub fn filter<'b>(&self, input: Cow<'b, str>) -> Cow<'b, str> {
+    match self.ammonia {
+      None => Cow::Owned(
+        ammonia::Builder::default().clean(&input).to_string()
+      ),
+      Some(ref sanitizer) => Cow::Owned(
+        sanitizer.clean(&input).to_string()
+      ),
+    }
+  }
+}
 
-  /// Whether to strip comments or not.
-  strip_comments: bool,
+impl<'a, 'b> FnOnce<(Cow<'b, str>, )> for StripTags<'a> {
+  type Output = Cow<'b, str>;
+
+  extern "rust-call" fn call_once(self, args: (Cow<'b, str>, )) -> Self::Output {
+    self.filter(args.0)
+  }
+}
+
+impl<'a, 'b> FnMut<(Cow<'b, str>, )> for StripTags<'a> {
+  extern "rust-call" fn call_mut(&mut self, args: (Cow<'b, str>, )) -> Self::Output {
+    self.filter(args.0)
+  }
+}
+
+impl<'a, 'b> Fn<(Cow<'b, str>, )> for StripTags<'a> {
+  extern "rust-call" fn call(&self, args: (Cow<'b, str>, )) -> Self::Output {
+    self.filter(args.0)
+  }
 }
