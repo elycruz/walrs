f2b4337eb1882ae4fc6425d23485b439e8da75d1
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2023-10-10 11:19:59 -0400

    issue-#3 - Progress on Equality validator impl updates, and tests.

--- inputfilter/src/validator/equal.rs
+++ inputfilter/src/validator/equal.rs
@@ -1,4 +1,3 @@
-use std::borrow::Cow;
 use std::fmt::Display;
 use crate::ToAttributesList;
 use crate::types::ConstraintViolation;
@@ -6,17 +5,17 @@ use crate::types::{InputValue, ValidateValue, ValidationResult};
 
 #[derive(Builder, Clone)]
 pub struct EqualityValidator<'a, T>
-  where T: InputValue
+  where T: InputValue + Clone
 {
-  pub rhs_value: Cow<'a, T>,
+  pub rhs_value: T,
   pub not_equal_msg: &'a (dyn Fn(&EqualityValidator<'a, T>, T) -> String + Send + Sync),
 }
 
 impl<'a, T> ValidateValue<T> for EqualityValidator<'a, T>
-  where T: InputValue,
+  where T: InputValue + Clone,
 {
   fn validate(&self, x: T) -> ValidationResult {
-    if x == *self.rhs_value {
+    if x == self.rhs_value {
       Ok(())
     } else {
       Err(vec![(ConstraintViolation::NotEqual, (self.not_equal_msg)(self, x))])
@@ -25,24 +24,46 @@ impl<'a, T> ValidateValue<T> for EqualityValidator<'a, T>
 }
 
 impl<T> Display for EqualityValidator<'_, T>
-  where T: InputValue,
+  where T: InputValue + Clone,
 {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
     write!(f, "EqualValidator {{rhs_value: {}}}", &self.rhs_value.to_string())
   }
 }
 
-impl<T: InputValue> ToAttributesList for EqualityValidator<'_, T> {
+impl<T: InputValue + Clone> ToAttributesList for EqualityValidator<'_, T> {
   fn to_attributes_list(&self) -> Option<Vec<(String, serde_json::Value)>> {
     Some(vec![(
       "pattern".to_string(),
-      serde_json::to_value(self.rhs_value.to_owned()).unwrap(),
+      serde_json::to_value(self.rhs_value.clone()).unwrap(),
     )])
   }
 }
 
-pub fn not_equal_msg<T>(_: &EqualityValidator<T>, value: T) -> String
+pub fn not_equal_msg<T: InputValue + Clone>(_: &EqualityValidator<T>, value: T) -> String
   where T: InputValue,
 {
   format!("Value must equal {}", value)
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  fn test_construction() {
+    let _ = EqualityValidatorBuilder::<&str>::default()
+      .rhs_value("foo".into())
+      .not_equal_msg(&not_equal_msg)
+      .build();
+  }
+
+  fn test_types() {
+    let _ = EqualityValidatorBuilder::<&str>::default()
+      .rhs_value("foo".into())
+      .not_equal_msg(&not_equal_msg)
+      .build();
+
+
+  }
 }
