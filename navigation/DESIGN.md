87cd8add575f8f6a71b7e3c6f2c67cc1f5ef46de
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2024-02-01 15:54:41 -0400

    navigation - Progress on initial implementation ideas [unstable] - Planning to go with a solution that just uses a symbol graph with some limitations.

---
+++ navigation/DESIGN.md
@@ -0,0 +1,8 @@
+# Design
+
+```pseudo
+- nav-item
+  - items
+    - nav-item
+      - items
+```
--- navigation/src/navigation.rs
+++ navigation/src/navigation.rs
@@ -1,24 +1,27 @@
 // use std::borrow::Cow;
+use serde_json;
 
 pub trait NavigationItem<'a> {
   // fn get_uri(&self) -> Option<Cow<'a, str>>;
   // fn get_label() -> Cow<'a, str>;
-  fn add(&mut self, item: NavItem) -> isize;
-  fn remove(&mut self, pred: &'a impl Fn(&'a NavItem) -> bool) -> Option<NavItem>;
+  fn add(&mut self, item: NavItem<'a>) -> isize;
+  fn remove(&mut self, pred: &'a impl Fn(&'a NavItem) -> bool) -> Option<NavItem<'a>>;
   fn find(&mut self, pred: &'a impl Fn(&'a NavItem) -> bool) -> Option<&'a NavItem>;
 
   /// Gets number of nav items in nav tree.
   fn size(&mut self) -> isize;
 }
 
-pub struct NavItem {
+#[derive(Builder)]
+pub struct NavItem<'a> {
   pub active: bool,
-  pub attributes: Option<Vec<String>>,
+  pub attributes: Option<Vec<(String, serde_json::Value)>>,
   pub children_only: bool,
   pub fragment: Option<String>,
-  pub items: Option<Vec<NavItem>>,
-  pub label: String,
+  pub items: Option<Vec<NavItem<'a>>>,
+  pub label: Option<String>,
   pub order: u64,
+  pub parent: Option<Box<&'a NavItem<'a>>>,
   pub privilege: Option<String>,
   pub resource: Option<String>,
   pub uri: Option<String>,
@@ -29,23 +32,34 @@ pub struct NavItem {
   _reevaluate_size: bool,
 }
 
-impl<'a> NavigationItem<'a> for NavItem {
+impl<'a> NavigationItem<'a> for NavItem<'a> {
   // fn get_uri(&self) -> Option<Cow<'a, str>> {
   //   self.uri.map(|uri| Cow::Borrowed(&uri.as_str()))
   // }
 
-  fn add(&mut self, item: NavItem) -> isize {
+  fn add(&mut self, mut item: NavItem<'a>) -> isize {
     self._reevaluate_size = true;
-    todo!()
+    item.parent = Some(Box::new(self));
+
+    if self.items.is_none() {
+      self.items = Some(vec![item]);
+    } else {
+      self.items.push(item);
+    }
+
+    self.size()
   }
 
   fn remove(&mut self, pred: &'a impl Fn(&'a NavItem) -> bool) -> Option<NavItem> {
     self._reevaluate_size = true;
+    // self.find(pred)d
     todo!()
   }
 
-  fn find(&mut self, pred: &'a impl Fn(&'a NavItem) -> bool) -> Option<&'a NavItem> {
-    todo!()
+  fn find(&mut self, pred: &'a impl Fn(&'a NavItem) -> bool) -> Option<Box<&'a NavItem>> {
+    self.items.map(|items| {
+      items.iter().find(pred).map(|x| Box::new(x))
+    }).flatten()
   }
 
   fn size(&mut self) -> isize {
