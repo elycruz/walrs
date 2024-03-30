9a6255c30c7e27fffdddae61ec6ae534e2ef0abb
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2024-03-30 14:43:26 -0400

    issue-#3 - Removed 'constraints' crate.

--- inputfilter/src/lib.rs
+++ inputfilter/src/lib.rs
@@ -8,11 +8,9 @@ extern crate derive_builder;
 pub mod traits;
 pub mod validators;
 pub mod filters;
-pub mod constraints;
 pub mod input;
 
 pub use traits::*;
 pub use validators::*;
 pub use filters::*;
-pub use constraints::*;
 pub use input::*;
--- inputfilter/src/traits.rs
+++ inputfilter/src/traits.rs
@@ -107,3 +107,24 @@ pub trait ToAttributesList {
     None
   }
 }
+
+pub type Filter<T> = dyn Fn(T) -> T + Send + Sync;
+
+pub type Validator<T> = dyn Fn(T) -> ValidationResult + Send + Sync;
+
+/// Violation message getter for `ValueMissing` Violation Enum type.
+pub type ValueMissingCallback = dyn Fn() -> ViolationMessage + Send + Sync;
+
+pub trait InputConstraints2<'a, 'b, T: 'b, FT: 'b>: Display + Debug
+  where T: InputValue {
+
+  fn validate(&self, value: Option<T>) -> Result<(), Vec<ViolationMessage>>;
+
+  fn validate_detailed(&self, value: Option<T>) -> Result<(), Vec<ViolationTuple>>;
+
+  fn filter(&self, value: FT) -> FT;
+
+  fn validate_and_filter(&self, value: Option<T>) -> Result<Option<FT>, Vec<ViolationMessage>>;
+
+  fn validate_and_filter_detailed(&self, value: Option<T>) -> Result<Option<FT>, Vec<ViolationTuple>>;
+}
