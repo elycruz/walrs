13ec6535c730b22a7d671ce5369de82c06a9f72a
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2024-05-23 00:10:28 -0400

    feat: issue#3 - input struct variant
    - Progress on 'Input1' struct variation -
      This verion of the struct should be able to
      accept 'ValidatorFn', and 'FilterFn' types.

--- walrs_inputfilter/tests/test_type_constructors.rs
+++ walrs_inputfilter/tests/test_type_constructors.rs
@@ -4,14 +4,15 @@ use std::borrow::Cow;
 use std::fmt::Display;
 use std::collections::HashMap;
 use serde::de::Error;
-use walrs_inputfilter::{Input, InputBuilder, InputConstraints, InputValue, ValidationResult, Validator, ViolationMessage, ViolationTuple};
+use walrs_inputfilter::{Input1, Input1Builder, InputConstraints, InputValue, ValidationResult, Validator, ViolationMessage, ViolationTuple};
 use walrs_inputfilter::ViolationEnum::{RangeOverflow, RangeUnderflow};
 
-/// Here we're setting up an application context example, for testing walrs_inputfilter types
+/// Here we're setting up an application context example, for testing walrs_inputfilter types,
+/// the way they might occur in an application.
 
 type ModelValidationResult = Result<(), HashMap<&'static str, Vec<ViolationMessage>>>;
 
-// Fixes error in issue #56105 (see the issue on rust github for more)
+// Fixes error in issue #56105 (see the issue on rust GitHub for more)
 trait Shush56105<T: ?Sized>: Fn(&T) -> ValidationResult {}
 impl<T: ?Sized, F: ?Sized> Shush56105<T> for F
     where
@@ -25,11 +26,13 @@ struct SomeModel {
     name: String,
     id: usize,
 }
+type VFn<T> = Validator<T>;
+type FFn<T> = ValidatorRef<T>;
 
 /// Filter rules
 struct InputRules<'a, 'b> {
-    name: Input<'a, &'b str, Cow<'b, str>>,
-    id: Input<'a, usize, usize>
+    name: Input1<'a, &'b str, Cow<'b, str>, VFn<&'b str>, FFn<Cow<'b, str>>>,
+    id: Input1<'a, usize>
 }
 
 impl SomeModel {
@@ -61,8 +64,9 @@ fn app_runtime_ctx(rules: &InputRules, data: String) {
 
 #[test]
 fn test_type_constructors() -> Result<(), Box<dyn Error>> {
-    let name: Input<&str, Cow<str>> = Input::new();
-    let id: Input<usize, usize> = InputBuilder::<usize, usize>::default()
+    let name: Input1<&str, Cow<str>> = Input1::new();
+
+    let id: Input1<usize, usize> = Input1Builder::default()
         .validators(vec![
             &|n| if n == 0 { Err(vec![(RangeUnderflow, "range-underflow error".to_string())]) }
             else if n > 10 { Err(vec![(RangeOverflow, "range-overflow error".to_string())]) }
