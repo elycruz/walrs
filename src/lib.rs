daef6c8ee5e8d58bb19184bf998acb462de29081
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2025-02-06 17:44:03 -0400

    feat: issue-#3 - inputfilter dir
    - Moved 'walrs_inputfilter/' back
      to 'inputfilter'.

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,4 @@ pub use walrs_inputfilter::input;
 pub use walrs_inputfilter::ref_input;
 pub use walrs_inputfilter::traits;
 pub use walrs_inputfilter::validators;
+pub use walrs_inputfilter::violation;
