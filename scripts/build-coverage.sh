5f831de042cd505d91a09367d21fcea443d5007c
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2025-09-08 14:34:40 -0400

    feat: issue-#3 - coverage options

--- scripts/build-coverage.sh
+++ scripts/build-coverage.sh
@@ -1,5 +1,7 @@
 rm -rf ./target/coverage/html && \
-CARGO_INCREMENTAL=0 RUSTFLAGS='-Cinstrument-coverage' LLVM_PROFILE_FILE='target/.profraw/cargo-test-%p-%m.profraw' \
+CARGO_INCREMENTAL=0 \
+RUSTFLAGS='-Cinstrument-coverage -Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort' \
+LLVM_PROFILE_FILE='target/.profraw/cargo-test-%p-%m.profraw' \
 grcov . --binary-path ./target/debug/deps/ -s . -t html --branch --ignore-not-existing \
 --ignore '_recycler' --ignore 'src/**/*' --ignore "target/debug/build/**/*" -o ./target/coverage/html \
 --excl-line "unreachable!" \
