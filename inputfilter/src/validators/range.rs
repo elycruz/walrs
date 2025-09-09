3b0ecf0faa463e40f1fb3693384ef83f88942465
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2025-09-09 17:38:11 -0400

    feat: issue-#3 - range vldtr cov

--- inputfilter/src/validators/range.rs
+++ inputfilter/src/validators/range.rs
@@ -224,19 +224,39 @@ mod test {
   use crate::ViolationType::{RangeOverflow, RangeUnderflow};
 
   #[test]
+  fn test_new_and_default() {
+    let input = RangeValidator::<usize>::new();
+
+    // Assert defaults
+    // ----
+    assert_eq!(input.min, None);
+    assert_eq!(input.max, None);
+
+    let input_def = RangeValidator::<usize>::default();
+
+    // Assert defaults
+    // ----
+    assert_eq!(input_def.min, None);
+    assert_eq!(input_def.max, None);
+  }
+
+  #[test]
   fn test_validate() {
     // Setup validators and test cases
     // ---
     let usize_required = RangeValidatorBuilder::<usize>::default()
-      .min(1)
-      .max(10)
-      .build()
-      .unwrap();
+        .min(1)
+        .max(10)
+        .build()
+        .unwrap();
+
+    let empty_vldtr = RangeValidator::<usize>::new();
 
     let test_cases = [
       ("With valid value", &usize_required, 1, Ok(())),
       ("With valid value", &usize_required, 4, Ok(())),
       ("With valid value", &usize_required, 10, Ok(())),
+      ("With valid value", &empty_vldtr, 10, Ok(())),
       (
         "With \"out of lower bounds\" value",
         &usize_required,
@@ -261,6 +281,48 @@ mod test {
     for (i, (test_name, input, value, expected_rslt)) in test_cases.into_iter().enumerate() {
       println!("Case {}: {}", i + 1, test_name);
       assert_eq!(input.validate(value), expected_rslt);
+      assert_eq!((&input)(value), expected_rslt);
     }
+  }
+
+  #[test]
+  fn test_fn_traits() {
+    let vldtr = RangeValidatorBuilder::<i32>::default()
+        .build()
+        .unwrap();
+
+    fn call_fn_once<T: ScalarValue>(f: impl FnOnce(T) -> ValidatorResult, v: T) -> ValidatorResult {
+      f(v)
+    }
+
+    fn call_fn_mut<T: ScalarValue>(f: &mut impl FnMut(T) -> ValidatorResult, v: T) -> ValidatorResult {
+      f(v)
+    }
+
+    // Test FnMut
+    let mut vldtr_mut = vldtr.clone();
+    let result_mut = call_fn_mut(&mut vldtr_mut, 99);
+    assert_eq!(result_mut, Ok(()));
+
+    // Test FnOnce
+    let result_once = call_fn_once(vldtr, 99);
+    assert_eq!(result_once, Ok(()));
+  }
+
+  #[test]
+  fn test_display_and_debug() {
+    let vldtr = RangeValidatorBuilder::<i32>::default()
+      .min(1)
+      .max(10)
+      .build()
+      .unwrap();
+
+    let display_output = format!("{}", vldtr);
+    let debug_output = format!("{:?}", vldtr);
+
+    let expected_output = "RangeValidator { min: 1, max: 10 }";
+
+    assert_eq!(display_output, expected_output);
+    assert_eq!(debug_output, expected_output);
   }
 }
