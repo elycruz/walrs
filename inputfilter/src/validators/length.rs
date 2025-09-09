b1be0358e9f5b0b80a25314e9b89669bb941ae01
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2025-09-09 17:04:44 -0400

    feat: issue-#3 - length coverage
    - README coverage notes.
    - test scripts and
      'length' validator
      coverage.

--- inputfilter/src/validators/length.rs
+++ inputfilter/src/validators/length.rs
@@ -2,12 +2,60 @@ use crate::{ValidateRef, ValidatorResult, Violation, ViolationMessage, Violation
 use std::collections::{BTreeMap, BTreeSet, HashMap, HashSet, VecDeque};
 
 pub trait WithLength {
-  fn length(&self) -> Option<usize>;
+  fn length(&self) -> usize;
 }
 
 pub type LengthValidatorCallback<'a, T> =
   dyn Fn(&LengthValidator<'a, T>, &T) -> ViolationMessage + Send + Sync;
 
+/// Validates the length of a value with a length (strings, collections, etc.).
+///
+/// ```rust
+/// use walrs_inputfilter::{
+///  len_too_long_msg,
+///  len_too_short_msg,
+///  Validate,
+///  ValidateRef, // Accessed regardless of whether using `validate_ref` or `Fn*` traits.
+///  LengthValidator,
+///  LengthValidatorBuilder,
+///  Violation,
+///  ViolationType
+/// };
+///
+/// let len_one_to_ten = LengthValidatorBuilder::<str>::default()
+///  .min_length(1)
+///  .max_length(10)
+///  .too_short_msg(&len_too_short_msg) // optional
+///  .too_long_msg(&len_too_long_msg)   // optional
+///  .build()
+///  .unwrap();
+///
+///  assert!(len_one_to_ten.validate_ref("hello").is_ok());
+///
+///  // Using Fn traits:
+///  assert!((&len_one_to_ten)("hello").is_ok());
+///  assert!((&len_one_to_ten)("").is_err()); // too short
+///  assert!((&len_one_to_ten)("this string is way too long").is_err());
+///
+/// // Collection example:
+/// // ----
+/// let array_len = LengthValidatorBuilder::<[i32]>::default()
+///   .min_length(2)
+///   .max_length(5)
+///   .build()
+///   .unwrap();
+///
+/// assert_eq!(array_len.validate_ref(&[1, 2, 3]), Ok(()));
+/// assert_eq!((&array_len)(&[1, 2, 3]), Ok(()));
+/// assert_eq!((&array_len)(&[1]), Err(Violation(
+///   ViolationType::TooShort,
+///   "Value length `1` is less than allowed minimum `2`.".to_string()
+/// )));
+/// assert_eq!((&array_len)(&[1, 2, 3, 4, 5, 6]), Err(Violation(
+///   ViolationType::TooLong,
+///   "Value length `6` is greater than allowed maximum `5`.".to_string()
+/// )));
+/// ```
 #[derive(Builder, Clone)]
 #[builder(pattern = "owned", setter(strip_option))]
 pub struct LengthValidator<'a, T>
@@ -39,8 +87,8 @@ impl<'a, T: WithLength + ?Sized> LengthValidator<'a, T> {
 macro_rules! validate_type_with_chars {
   ($type_:ty) => {
     impl WithLength for $type_ {
-      fn length(&self) -> Option<usize> {
-        Some(self.chars().count() as usize)
+      fn length(&self) -> usize {
+        self.chars().count() as usize
       }
     }
   };
@@ -56,8 +104,8 @@ macro_rules! validate_type_with_len {
     };
     ($type_:ty, $($generic:ident),*$(,)*) => {
         impl<$($generic),*> WithLength for $type_ {
-            fn length(&self) -> Option<usize> {
-                Some(self.len() as usize)
+            fn length(&self) -> usize {
+                self.len() as usize
             }
         }
     };
@@ -78,7 +126,7 @@ validate_type_with_len!(VecDeque<T>, T);
 // validate_type_with_len!(IndexMap<K, V>, K, V);
 
 // ====
-// /End of validator_types crate rip:
+// /End of validator_types crate rip.
 // ====
 
 impl<'a, T> ValidateRef<T> for LengthValidator<'a, T>
@@ -118,23 +166,23 @@ where
   /// }
   /// ```
   fn validate_ref(&self, value: &T) -> ValidatorResult {
-    if let Some(len) = value.length() {
-      if let Some(min_length) = self.min_length {
-        if len < min_length {
-          return Err(Violation(
-            ViolationType::TooShort,
-            (self.too_short_msg)(self, value),
-          ));
-        }
-      }
+    let len = value.length();
 
-      if let Some(max_length) = self.max_length {
-        if len > max_length {
-          return Err(Violation(
-            ViolationType::TooLong,
-            (self.too_long_msg)(self, value),
-          ));
-        }
+    if let Some(min_length) = self.min_length {
+      if len < min_length {
+        return Err(Violation(
+          ViolationType::TooShort,
+          (self.too_short_msg)(self, value),
+        ));
+      }
+    }
+
+    if let Some(max_length) = self.max_length {
+      if len > max_length {
+        return Err(Violation(
+          ViolationType::TooLong,
+          (self.too_long_msg)(self, value),
+        ));
       }
     }
 
@@ -163,6 +211,16 @@ impl<'a, T: WithLength + ?Sized> Fn<(&T,)> for LengthValidator<'a, T> {
 }
 
 impl<'a, T: WithLength + ?Sized> Default for LengthValidator<'a, T> {
+  /// Creates a `LengthValidator` with no constraints.
+  ///
+  /// ```rust
+  /// use walrs_inputfilter::LengthValidator;
+  ///
+  /// let default_vldtr = LengthValidator::<str>::default();
+  ///
+  /// assert!(default_vldtr.min_length.is_none());
+  /// assert!(default_vldtr.max_length.is_none());
+  /// ```
   fn default() -> Self {
     LengthValidator::<T>::new()
   }
@@ -174,7 +232,7 @@ pub fn len_too_short_msg<'a, T: WithLength + ?Sized>(
 ) -> String {
   format!(
     "Value length `{}` is less than allowed minimum `{}`.",
-    xs.length().unwrap_or(0),
+    xs.length(),
     &rules.min_length.unwrap_or(0)
   )
 }
@@ -185,7 +243,7 @@ pub fn len_too_long_msg<'a, T: WithLength + ?Sized>(
 ) -> String {
   format!(
     "Value length `{}` is greater than allowed maximum `{}`.",
-    xs.length().unwrap_or(0),
+    xs.length(),
     &rules.max_length.unwrap_or(0)
   )
 }
@@ -199,13 +257,25 @@ mod test {
   };
 
   #[test]
-  fn test_validate_ref() -> Result<(), Box<dyn std::error::Error>> {
+  fn test_validate_ref() {
     let no_rules = LengthValidator::<str>::new();
     let len_one_to_ten = LengthValidatorBuilder::<str>::default()
       .min_length(1)
       .max_length(10)
       .build()
       .unwrap();
+
+    let gte_than_8 = LengthValidatorBuilder::<str>::default()
+      .min_length(8)
+      .build()
+      .unwrap();
+
+    let ngte_than_8 = LengthValidatorBuilder::<str>::default()
+      .max_length(8)
+      .build()
+      .unwrap();
+
+    let no_rules = LengthValidator::<str>::new();
 
     let too_long_str = "12345678901";
     let just_right_str = &too_long_str[1..];
@@ -229,13 +299,76 @@ mod test {
       ),
       ("Value just right (1)", &len_one_to_ten, "a", Ok(())),
       ("Value just right", &len_one_to_ten, just_right_str, Ok(())),
+      (">= 8 - pass", &gte_than_8, "12345678", Ok(())),
+      (">= 8 - fail", &gte_than_8, "1234567", Err(Violation(TooShort, len_too_short_msg(&gte_than_8, "1234567")))),
+      ("No rules - empty str", &no_rules, "", Ok(())),
+      ("No rules - long str", &no_rules, "this string is way too long", Ok(())),
     ];
 
     for (name, rules, value, expected) in test_cases {
       assert_eq!(rules.validate_ref(value), expected, "{}", name);
       assert_eq!(rules(value), expected, "{}", name);
     }
+  }
 
-    Ok(())
+  #[test]
+  fn test_with_collection_types() {
+    let len_two_to_five = LengthValidatorBuilder::<[i32]>::default()
+      .min_length(2)
+      .max_length(5)
+      .build()
+      .unwrap();
+
+    assert_eq!(len_two_to_five.validate_ref(&[1, 2, 3]), Ok(()));
+    assert_eq!((&len_two_to_five)(&[1, 2, 3]), Ok(()));
+    assert_eq!(
+      (&len_two_to_five)(&[1]),
+      Err(Violation(
+        TooShort,
+        "Value length `1` is less than allowed minimum `2`.".to_string()
+      ))
+    );
+    assert_eq!(
+      (&len_two_to_five)(&[1, 2, 3, 4, 5, 6]),
+      Err(Violation(
+        TooLong,
+        "Value length `6` is greater than allowed maximum `5`.".to_string()
+      ))
+    );
+  }
+
+  #[test]
+  fn test_default_and_new() {
+    let default_vldtr = LengthValidator::<str>::default();
+
+    assert!(default_vldtr.min_length.is_none());
+    assert!(default_vldtr.max_length.is_none());
+
+    let new_vldtr = LengthValidator::<str>::new();
+
+    assert!(new_vldtr.min_length.is_none());
+    assert!(new_vldtr.max_length.is_none());
+  }
+
+  #[test]
+  fn test_fn_traits() {
+    let mut vldtr = LengthValidatorBuilder::<str>::default()
+      .min_length(2)
+      .max_length(5)
+      .build()
+      .unwrap();
+
+    fn call_fn_once(v: impl FnOnce(&str) -> ValidatorResult, s: &str) -> ValidatorResult
+    {
+      v(s)
+    }
+
+    fn call_fn_mut(v: &mut impl FnMut(&str) -> ValidatorResult, s: &str) -> ValidatorResult
+    {
+      v(s)
+    }
+
+    assert_eq!(call_fn_mut(&mut vldtr, "abc"), Ok(()));
+    assert_eq!(call_fn_once(vldtr, "abc"), Ok(()));
   }
 }
--- test.sh
+++ test.sh
@@ -1,3 +1,1 @@
-CARGO_INCREMENTAL=0 RUSTFLAGS='-Cinstrument-coverage' LLVM_PROFILE_FILE='target/.profraw/cargo-test-%p-%m.profraw' \
-cargo test --workspace && \
-sh ./scripts/build-coverage.sh
+cargo llvm-cov --html --workspace --branch
