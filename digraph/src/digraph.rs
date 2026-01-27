3c6f4ddb52974bb5b9f2c8bbb771318c1f2f52f9
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2026-01-26 20:22:23 -0500

    feat: issue-#38 - digraph coverage
    - 'TryFrom' impl tests.

--- digraph/src/digraph.rs
+++ digraph/src/digraph.rs
@@ -227,7 +227,9 @@ impl TryFrom<File> for Digraph {
 
 #[cfg(test)]
 mod test {
-  use crate::{invalid_vertex_msg, Digraph};
+  use std::fs::File;
+  use std::io::{BufReader, Seek};
+  use crate::{extract_vert_and_edge_counts_from_bufreader, invalid_vertex_msg, Digraph};
 
   /// Some usize vec;  Note not meant to be generic;  Defined only this `test` module.
   pub fn usize_vec_sum(us: &Vec<usize>) -> usize {
@@ -489,6 +491,76 @@ mod test {
         );
       }
     }
+
+    Ok(())
+  }
+
+  #[test]
+  pub fn test_try_from_file_ref() -> Result<(), std::io::Error> {
+    let file_path = "../test-fixtures/graph_test_tinyG.txt";
+
+    // Get digraph data
+    let f = File::open(&file_path)?;
+
+    // Create graph
+    let _: Digraph = (&f).try_into().unwrap();
+
+    Ok(())
+  }
+
+  #[test]
+  pub fn test_try_from_file() -> Result<(), std::io::Error> {
+    let file_path = "../test-fixtures/graph_test_tinyG.txt";
+
+    // Get digraph data
+    let f = File::open(&file_path)?;
+
+    // Create graph
+    let _: Digraph = f.try_into().unwrap();
+
+    Ok(())
+  }
+
+  #[test]
+  pub fn test_try_from_mut_buf_reader_ref() -> Result<(), Box<dyn std::error::Error>> {
+    let file_path = "../test-fixtures/graph_test_tinyG.txt";
+
+    // Get digraph data
+    let f = File::open(&file_path)?;
+    let mut reader = BufReader::new(f);
+
+    // Create graph (impls for `From<BufReader<R: std::io::Read>>` and `From<File>` are defined for `Digraph` struct
+    let dg: Digraph = (&mut reader).try_into()?;
+
+    // Rewind reader and extract vert and edge count from first lines
+    reader.rewind()?;
+
+    let (expected_vert_count, expected_edge_count) =
+        extract_vert_and_edge_counts_from_bufreader(&mut reader)?;
+
+    assert_eq!(
+      dg.vert_count(),
+      expected_vert_count,
+      "Vert count is invalid"
+    );
+    assert_eq!(
+      dg.edge_count(),
+      expected_edge_count,
+      "Edge count is invalid"
+    );
+
+    Ok(())
+  }
+
+  #[test]
+  pub fn test_try_from_buf_reader() -> Result<(), std::io::Error> {
+    let file_path = "../test-fixtures/graph_test_tinyG.txt";
+
+    // Get digraph data
+    let f = File::open(&file_path)?;
+
+    // Create graph
+    let _: Digraph = BufReader::new(f).try_into().unwrap();
 
     Ok(())
   }
