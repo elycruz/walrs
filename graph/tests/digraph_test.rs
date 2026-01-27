7ce642bb18eaacf6c011873d682cf77af666443a
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2026-01-26 20:06:16 -0500

    feat: issue-#38 - digraph coverage
    - e2e tests cleanup.

--- graph/tests/digraph_test.rs
+++ graph/tests/digraph_test.rs
@@ -6,7 +6,7 @@ mod test {
   use walrs_graph::graph::shared_utils::extract_vert_and_edge_counts_from_bufreader;
 
   #[test]
-  pub fn test_from_file_ref_impl() -> Result<(), std::io::Error> {
+  pub fn test_try_from_file_ref() -> Result<(), std::io::Error> {
     let file_path = "../test-fixtures/graph_test_tinyG.txt";
 
     // Get digraph data
@@ -15,13 +15,24 @@ mod test {
     // Create graph
     let _: Digraph = (&f).try_into().unwrap();
 
-    // println!("{:?}", dg);
+    Ok(())
+  }
+
+  #[test]
+  pub fn test_try_from_file() -> Result<(), std::io::Error> {
+    let file_path = "../test-fixtures/graph_test_tinyG.txt";
+
+    // Get digraph data
+    let f = File::open(&file_path)?;
+
+    // Create graph
+    let _: Digraph = f.try_into().unwrap();
 
     Ok(())
   }
 
   #[test]
-  pub fn test_from_mut_ref_buf_reader_impl() -> Result<(), Box<dyn std::error::Error>> {
+  pub fn test_try_from_mut_buf_reader_ref() -> Result<(), Box<dyn std::error::Error>> {
     let file_path = "../test-fixtures/graph_test_tinyG.txt";
 
     // Get digraph data
@@ -30,8 +41,6 @@ mod test {
 
     // Create graph (impls for `From<BufReader<R: std::io::Read>>` and `From<File>` are defined for `Digraph` struct
     let dg: Digraph = (&mut reader).try_into()?;
-
-    // println!("{:?}", dg);
 
     // Rewind reader and extract vert and edge count from first lines
     reader.rewind()?;
@@ -54,40 +63,7 @@ mod test {
   }
 
   #[test]
-  pub fn test_try_from_mut_ref_buf_reader_impl() -> Result<(), Box<dyn std::error::Error>> {
-    let file_path = "../test-fixtures/graph_test_tinyG.txt";
-
-    // Get digraph data
-    let f = File::open(&file_path)?;
-    let mut reader = BufReader::new(f);
-
-    // Create graph (impls for `From<BufReader<R: std::io::Read>>` and `From<File>` are defined for `Digraph` struct
-    let dg: Digraph = (&mut reader).try_into()?;
-
-    // println!("{:?}", dg);
-
-    // Rewind reader and extract vert and edge count from first lines
-    reader.rewind()?;
-
-    let (expected_vert_count, expected_edge_count) =
-      extract_vert_and_edge_counts_from_bufreader(&mut reader)?;
-
-    assert_eq!(
-      dg.vert_count(),
-      expected_vert_count,
-      "Vert count is invalid"
-    );
-    assert_eq!(
-      dg.edge_count(),
-      expected_edge_count,
-      "Edge count is invalid"
-    );
-
-    Ok(())
-  }
-
-  #[test]
-  pub fn test_from_buf_reader_impl() -> Result<(), std::io::Error> {
+  pub fn test_try_from_buf_reader() -> Result<(), std::io::Error> {
     let file_path = "../test-fixtures/graph_test_tinyG.txt";
 
     // Get digraph data
@@ -95,8 +71,6 @@ mod test {
 
     // Create graph
     let _: Digraph = BufReader::new(f).try_into().unwrap();
-
-    // println!("{:?}", dg);
 
     Ok(())
   }
