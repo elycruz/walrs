8262aa9f3864cea5ac389b87f51ea7b32b71a153
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2026-02-02 10:51:29 -0500

    feat: issue-#38,issue-#39 - depth first order impl

--- digraph/src/lib.rs
+++ digraph/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod digraph;
 pub mod digraph_dicycle;
 pub mod digraph_dipaths_dfs;
+pub mod depth_first_order;
 pub mod math;
 pub mod disymgraph;
 pub mod utils;
@@ -9,6 +10,7 @@ pub mod traits;
 pub use digraph::*;
 pub use digraph_dicycle::*;
 pub use digraph_dipaths_dfs::*;
+pub use depth_first_order::*;
 pub use math::*;
 pub use disymgraph::*;
 pub use utils::*;
