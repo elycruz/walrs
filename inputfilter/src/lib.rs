ac7cb7202ac08536383e00556f97e351d2bf8632
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2026-02-15 12:31:37 -0500

    feat: #3 - rules mod
    - 'Rule<T>' type and it's
      related enums.

--- inputfilter/src/lib.rs
+++ inputfilter/src/lib.rs
@@ -9,6 +9,7 @@ pub mod filters;
 pub mod input;
 pub(crate) mod input_common;
 pub mod ref_input;
+pub mod rule;
 pub mod traits;
 pub mod validators;
 
@@ -18,6 +19,7 @@ pub use walrs_validator::{Violation, Violations, ViolationType, ViolationMessage
 pub use filters::*;
 pub use input::*;
 pub use ref_input::*;
+pub use rule::{Condition, Rule, RuleResult};
 pub use traits::*;
 pub use validators::*;
 
