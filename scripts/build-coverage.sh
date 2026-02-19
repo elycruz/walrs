b7c0763003c55fbdafab9260ac29b1134f85d143
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2026-02-19 02:34:28 -0500

    chore: cleanup-cov-scripts

--- scripts/build-coverage.sh
+++
@@ -1,8 +0,0 @@
-rm -rf ./target/coverage/html && \
-CARGO_INCREMENTAL=0 \
-RUSTFLAGS='-Cinstrument-coverage -Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort' \
-LLVM_PROFILE_FILE='target/.profraw/cargo-test-%p-%m.profraw' \
-grcov . --binary-path ./target/debug/deps/ -s . -t html --branch --ignore-not-existing \
---ignore '_recycler' --ignore 'src/**/*' --ignore "target/debug/build/**/*" -o ./target/coverage/html \
---excl-line "unreachable!" \
- && echo "Process completed successfully.\nCoverage generated to ./target/coverage/html\n"
